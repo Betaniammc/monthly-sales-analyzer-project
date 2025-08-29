ed72edf2ab0303b85174957742e56d0faa43f9c0
Betaniammc <126913973+Betaniammc@users.noreply.github.com>
2025-08-29 11:14:27 +0000

    Complete exercises

--- monthly_sales_analyzer.py
+++ monthly_sales_analyzer.py
@@ -24,33 +24,100 @@ sales_data = [
 
 def total_sales_by_product(data, product_key):
     """Calculates the total sales of a specific product in 30 days."""
+    total = 0
+    for ventas in data:
+        total += ventas[product_key]
+    return total
     pass
 
 
 def average_daily_sales(data, product_key):
     """Calculates the average daily sales of a specific product."""
+    T_average = total_sales_by_product(data, product_key)/len(data)
+    return T_average
     pass
 
 
 def best_selling_day(data):
     """Finds the day with the highest total sales."""
+    total=0
+    dia=0
+    for ventas in data:
+        sum_day = sum(v for k, v in ventas.items() if k != "day")
+        if sum_day > total:
+            total=sum_day
+            dia = ventas["day"]
+    return dia
     pass
 
 
 def days_above_threshold(data, product_key, threshold):
     """Counts how many days the sales of a product exceeded a given threshold."""
+    total = 0
+    for ventas in data:
+        if (ventas[product_key]>threshold):
+            total +=1
+    return total
     pass
 
 
 def top_product(data):
     """Determines which product had the highest total sales in 30 days."""
+    sum_productos = {}
+    for ventas in data:
+        for produc, cant in ventas.items():
+            if produc != "day":
+                sum_productos[produc]=sum_productos.get(produc, 0) + cant
+    max_product = max(sum_productos, key=sum_productos.get)
+    return max_product    
     pass
 
 
+def wors_selling_day(data):
+    """Finds the day with the wors total sales."""
+    total=sum(v for k, v in data[0].items() if k != "day")
+    dia = 1
+    for ventas in data:
+        sum_day = sum(v for k, v in ventas.items() if k != "day")
+        if sum_day < total:
+            total=sum_day
+            dia = ventas["day"]
+    return dia
+    pass
 
+def selling_day(data):
+    """Finds the total sales."""
+    selling_per_day = []
+    total=0
+    dia=0
+    for ventas in data:
+        sum_day = sum(v for k, v in ventas.items() if k != "day")
+        selling_per_day.append({"day" : ventas["day"] , "total_selling" : sum_day})
+    selling_day = sorted(selling_per_day, key=lambda x: x["total_selling"])
+    return selling_day[-3:]
+    pass
+
+def range_max_min_selling(data, product_key):
+
+    """Finds the range of salling of a product."""
+    wors_salling = data[0][product_key]
+    best_salling = 0
+
+    for ventas in data:
+        if ventas[product_key]>best_salling:
+            best_salling = ventas[product_key]
+        else:
+            wors_salling = ventas[product_key]    
+
+    range= best_salling-wors_salling   
+    return (f"wors_sales: {wors_salling}, best_sales: {best_salling}, range: {range}")
+    
 # Function tests
 print("Total sales of product_a:", total_sales_by_product(sales_data, "product_a"))
 print("Average daily sales of product_b:", average_daily_sales(sales_data, "product_b"))
 print("Day with highest total sales:", best_selling_day(sales_data))
 print("Days when product_c exceeded 300 sales:", days_above_threshold(sales_data, "product_c", 300))
 print("Product with highest total sales:", top_product(sales_data))
+print("Day with wors total sales:", wors_selling_day(sales_data))
+print("three Days with best total sales:", selling_day(sales_data))
+print("the sales total: ",range_max_min_selling(sales_data, "product_a"))
